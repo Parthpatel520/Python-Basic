73591b00d09bd54da8a9bb6473cc797cc227242c
Parthpatel520 <130030126+Parthpatel520@users.noreply.github.com>
2026-02-16 11:45:38 +0530

    Add files via upload

---
+++ Modules/math_Tool.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def hello():
+    print("Hello from Mymath!")
+    
+hello()
+
+
+
+if __name__ == "__main__":
+    # This runs only if we execute this file directly
+    print("Testing math functions:")
+    print("Add:", add(2, 3))
+    print("Multiply:", multiply(2, 3))
