cfca4eefc994735366a261fbb0acb4799e813b1f
Parthpatel520 <130030126+Parthpatel520@users.noreply.github.com>
2026-03-10 11:56:05 +0530

    Add files via upload

---
+++ gRPC/client.py
@@ -0,0 +1,19 @@
+import grpc
+import hello_pb2
+import hello_pb2_grpc
+
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+
+    stub = hello_pb2_grpc.GreeterStub(channel)
+
+    response = stub.SayHello(
+        hello_pb2.HelloRequest(name="Parth")
+    )
+
+    print("Server response:", response.message)
+
+
+if __name__ == "__main__":
+    run()
