32f5be20a612c0e509c56fc1b71a5efaac4c4df4
Parthpatel520 <130030126+Parthpatel520@users.noreply.github.com>
2026-03-10 18:41:48 +0530

    Modify SMS message and add credentials
    Updated SMS content and replaced placeholders with actual account SID and auth token.

--- Twilio and Mailgun/notification_demo/send_sms.py
+++ Twilio and Mailgun/notification_demo/send_sms.py
@@ -1,20 +1,23 @@
 from twilio.rest import Client
 
-account_sid = "YOUR_ACCOUNT_SID"
-auth_token = "YOUR_AUTH_TOKEN"
+account_sid = "ACaa01dd0e45663f822ce483a24e6a2490"
+auth_token = "d1a70d59807469ed82c694276835d58d"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    body="""Hello Mr. Kunj Chhatrala!
-
-If you are currently working, please reply with Code 1.
-If you are watching Instagram reels, please switch off your phone.
-
-- Parth Chhatrala.
-""",
-    from_="+17752627678",
-    to="+919106467043"
+      body="""Hello Mr. Kunj Chhatrala!,
+      
+        If you are currently working, please reply with Code 1.
+        If you are watching Instagram reels, please switch off your phone.
+          - Parth Chhatrala.""",
+        
+    from_="+1 775 262 7678",     # Twilio number
+    to="+919106467043"       # Your number
 )
 
 print("SMS Sent:", message.sid)
+
+
+# twilio = MQ9K8SEDBAYCVCQX5Q7Z3M19
+
